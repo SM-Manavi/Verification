676ac1631855e3328ea9911b315fc04ea142456a
SM-Manavi <55019725+SM-Manavi@users.noreply.github.com>
2019-09-08 11:58:09 +0430

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include "dispatch.h"
+#include <Windows.h>
+
+int main()
+{
+    Environment state(Rect_2(Point_2(200,200),Point_2(-200,-200)), 10);
+
+    state.add_robot(Point_2(-1, 4));
+    state.add_robot(Point_2(0, 6));
+	state.add_robot(Point_2(6, 0));
+
+    state.initialization();
+
+    Dispatch dis(&state);
+	dis.Turn_taking();
+
+    state.createLTS();
+
+	int d;
+	std::cin >> d;
+}
+
+
+
+
+
+
+
